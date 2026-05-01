c06c9580fb95c9820bbb439ed7e94d6de31f9b06
Jellysquadz <jellysquadz@gmail.com>
2026-05-01 13:56:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 window.onload = function () {
   const img = document.getElementById('bouncing-image');
 
+  if (!img) {
+    console.error("Image not found");
+    return;
+  }
+
   let x = 50;
   let y = 50;
 
@@ -12,19 +17,19 @@ window.onload = function () {
     const imgWidth = img.clientWidth;
     const imgHeight = img.clientHeight;
 
+    if (!imgWidth || !imgHeight) {
+      requestAnimationFrame(moveImage);
+      return;
+    }
+
     const windowWidth = window.innerWidth;
     const windowHeight = document.documentElement.clientHeight;
 
     x += dx;
     y += dy;
 
-    if (x + imgWidth >= windowWidth || x <= 0) {
-      dx *= -1;
-    }
-
-    if (y + imgHeight >= windowHeight || y <= 0) {
-      dy *= -1;
-    }
+    if (x + imgWidth >= windowWidth || x <= 0) dx *= -1;
+    if (y + imgHeight >= windowHeight || y <= 0) dy *= -1;
 
     img.style.transform = `translate(${x}px, ${y}px)`;
 
